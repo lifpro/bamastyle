64e43d88a4f272d4951f0fb8f613a69180948855
mariasoftmali <mariasoft68@gmail.com>
2022-02-18 19:26:06 +0000

    photo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   models: { id: number, nom: string, prix: number, photo: string }[] = [
-    { id: 1, nom: "Abaya", prix: 25000, photo: "photo1" },
-    { id: 2, nom: "Taille basse", prix: 12500, photo: "photo2" },
-    { id: 3, nom: "Robe simple", prix: 5000, photo: "photo3" },
-    { id: 4, nom: "Veste 3 boutons", prix: 50000, photo: "photo4" },
-    { id: 5, nom: "Veste sans poche", prix: 35000, photo: "photo5" },
+    { id: 1, nom: "Abaya", prix: 25000, photo: "photo1.jpg" },
+    { id: 2, nom: "Taille basse", prix: 12500, photo: "photo2.jpg" },
+    { id: 3, nom: "Robe simple", prix: 5000, photo: "photo3.jpg" },
+    { id: 4, nom: "Veste 3 boutons", prix: 50000, photo: "photo4.jpg" },
+    { id: 5, nom: "Veste sans poche", prix: 35000, photo: "photo5.jpg" },
   ];
 
   constructor() { }
