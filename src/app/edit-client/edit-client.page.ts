725d6f46fd1f9595d4bdab245cd4f9437284685f
mariasoftmali <mariasoft68@gmail.com>
2022-03-04 18:32:18 +0000

    info client

--- src/app/edit-client/edit-client.page.ts
+++ src/app/edit-client/edit-client.page.ts
@@ -10,14 +10,27 @@ import { Clients } from '../models/clients';
 export class EditClientPage implements OnInit {
   idClient: string;
   cl: Clients;
-  clients: { id: number, nom: string, numero: number, adresse: string }[] = [
-    { id: 0, nom: "Aissata DIARRA", numero: 76887654, adresse: "Aci" },
-    { id: 1, nom: "Fatimata TOURE", numero: 76887654, adresse: "Sébénicoro" },
-    { id: 2, nom: "Fodé DIAKITE", numero: 76887654, adresse: "Hamdalaye" },
-    { id: 3, nom: "Adboulkarim SIDIBE", numero: 76887654, adresse: "Yirimadjo" },
-    { id: 4, nom: "Moussa DEMBELE", numero: 76887654, adresse: "Medina" },
-    { id: 5, nom: "Djénaba BA", numero: 76887654, adresse: "Missira" },
-  ];
+  clients: {
+    id: number, nom: string, numero: number,
+    adresse: string,
+    longeur: number,
+    epaule: number,
+    manche: number,
+    poitrine: number,
+    hanche: number,
+    cou: number,
+    bras: number,
+    poignet: number,
+    pantalon: number,
+    jupe: number
+  }[] = [
+      { id: 0, nom: "Aissata DIARRA", numero: 76887654, adresse: "Aci", longeur: 0, epaule: 0, manche: 0, poitrine: 0, hanche: 0, cou: 0, bras: 0, poignet: 0, pantalon: 0, jupe: 0 },
+      { id: 1, nom: "Fatimata TOURE", numero: 76887654, adresse: "Sébénicoro", longeur: 0, epaule: 0, manche: 0, poitrine: 0, hanche: 0, cou: 0, bras: 0, poignet: 0, pantalon: 0, jupe: 0 },
+      { id: 2, nom: "Fodé DIAKITE", numero: 76887654, adresse: "Hamdalaye", longeur: 0, epaule: 0, manche: 0, poitrine: 0, hanche: 0, cou: 0, bras: 0, poignet: 0, pantalon: 0, jupe: 0 },
+      { id: 3, nom: "Adboulkarim SIDIBE", numero: 76887654, adresse: "Yirimadjo", longeur: 0, epaule: 0, manche: 0, poitrine: 0, hanche: 0, cou: 0, bras: 0, poignet: 0, pantalon: 0, jupe: 0 },
+      { id: 4, nom: "Moussa DEMBELE", numero: 76887654, adresse: "Medina", longeur: 0, epaule: 0, manche: 0, poitrine: 0, hanche: 0, cou: 0, bras: 0, poignet: 0, pantalon: 0, jupe: 0 },
+      { id: 5, nom: "Djénaba BA", numero: 76887654, adresse: "Missira", longeur: 0, epaule: 0, manche: 0, poitrine: 0, hanche: 0, cou: 0, bras: 0, poignet: 0, pantalon: 0, jupe: 0 },
+    ];
 
   constructor(private route: ActivatedRoute) { }
 
